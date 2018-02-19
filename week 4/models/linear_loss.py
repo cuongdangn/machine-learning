da545e01386d4bc14ef98820d1b71c3d6dc22918
cuongdn97 <cuongdangngoc97@gmail.com>
2018-02-19 21:24:26 +0700

    no message

--- week 4/models/linear_loss.py
+++ week 4/models/linear_loss.py
@@ -20,18 +20,29 @@ def linear_loss_naive(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
-
+    N = y.size
+    D = X[0].size
+    # print(N," ",D)
     #############################################################################
-    # TODO: Compute the linear loss and its gradient using explicit loops.     #
+    # TODO: Compute the linear loss and its gradient using explicit loops.      #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
+    for i in range(N) :
+        temp_sum = 0
+        for j in range(D) :
+            temp_sum += X[i][j]*W[j]
+        for j in range(D) :
+            dW[j] += (temp_sum-y[i])*X[i][j]
+        loss = loss + np.power(temp_sum - y[i], 2)
+    loss = loss / (2*N) + reg
+    dW = np.dot(X.transpose(), np.dot(X,W)-y)
+    dW = dW / N
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
-
+    #print(dW)
     return loss, dW
 
 
@@ -44,16 +55,18 @@ def linear_loss_vectorized(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
-
+    N = y.size
     #############################################################################
     # TODO: Compute the linear loss and its gradient using no explicit loops.  #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization!                                                           #
     #############################################################################
-    pass
+    loss = np.sum((np.dot(X,W)-y)**2)/(2*N) + reg
+    dW = np.dot(X.transpose(), np.dot(X,W)-y)
+    dW = dW / N
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
-
+    #print(dW)
     return loss, dW
--- week 4/models/linear_regression.py
+++ week 4/models/linear_regression.py
@@ -3,88 +3,59 @@ from models.linear_loss import *
 
 class LinearRegression(object):
 
-  def __init__(self):
-    self.W = None
+    def __init__(self):  
+        self.W = None
 
-  def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100, verbose=False):
-    """
-    Train this linear regression classifier.
+    def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100, verbose=False):
+           
+        num_train, dim = X.shape
+        if self.W is None:
+          # lazily initialize W
+          self.W = 0.001 * np.random.randn(dim, 1)
 
-    Inputs:
-    - X: A numpy array of shape (N, D) containing training data; there are N
-      training samples each of dimension D.
-    - y: A numpy array of shape (N,) containing training labels; y[i] is a real number.
-    - learning_rate: (float) learning rate for optimization.
-    - reg: (float) regularization strength.
-    - num_iters: (integer) number of steps to take when optimizing
-    - verbose: (boolean) If true, print progress during optimization.
+        # Run gradient descent to optimize W
+        loss_history = []
+        for it in range(num_iters):
+            loss, grad = self.loss(X, y, reg)
+            loss_history.append(loss)
 
-    Outputs:
-    A list containing the value of the loss function at each training iteration.
-    """
-    num_train, dim = X.shape
-    if self.W is None:
-      # lazily initialize W
-      self.W = 0.001 * np.random.randn(dim, 1)
+          # perform parameter update
+          #########################################################################
+          # TODO:                                                                 #
+          # Update the weights using the gradient and the learning rate.          #
+          #########################################################################
+            self.W = self.W-learning_rate*grad
+          #########################################################################
+          #                       END OF YOUR CODE                                #
+          #########################################################################
+        if verbose and it % 100 == 0:
+            print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+        return loss_history
+    
+    def predict(self, X):
+    
+        y_pred = np.zeros(X.shape[1])
+        ###########################################################################
+        # TODO:                                                                   #
+        # Implement this method. Store the predicted labels in y_pred.            #
+        ###########################################################################
+        y_pred = np.dot(X,self.W)
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
+        return y_pred
 
-    # Run gradient descent to optimize W
-    loss_history = []
-    for it in range(num_iters):
+    def loss(self, X, y, reg):
+        """
+        Define the loss function with linear_loss_naive or linear_loss_vectorized
+        """
+        #########################################################################
+        # TODO:                                                                 #
+        # Calculate the loss value and gradient matrix with linear_loss_..      #
+        #########################################################################
+        loss, grad = linear_loss_vectorized(self.W,X,y,reg)
+        #########################################################################
+        #                       END OF YOUR CODE                                #
+        #########################################################################
 
-      # evaluate loss and gradient
-      loss, grad = self.loss(X, y, reg)
-      loss_history.append(loss)
-
-      # perform parameter update
-      #########################################################################
-      # TODO:                                                                 #
-      # Update the weights using the gradient and the learning rate.          #
-      #########################################################################
-      pass
-      #########################################################################
-      #                       END OF YOUR CODE                                #
-      #########################################################################
-
-      if verbose and it % 100 == 0:
-        print('iteration %d / %d: loss %f' % (it, num_iters, loss))
-
-    return loss_history
-
-  def predict(self, X):
-    """
-    Use the trained weights of this linear classifier to predict labels for
-    data points.
-
-    Inputs:
-    - X: D x N array of training data. Each column is a D-dimensional point.
-
-    Returns:
-    - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-      array of length N, and each element is an integer giving the predicted
-      class.
-    """
-    y_pred = np.zeros(X.shape[1])
-    ###########################################################################
-    # TODO:                                                                   #
-    # Implement this method. Store the predicted labels in y_pred.            #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                           END OF YOUR CODE                              #
-    ###########################################################################
-    return y_pred
-
-  def loss(self, X, y, reg):
-    """
-    Define the loss function with linear_loss_naive or linear_loss_vectorized
-    """
-    #########################################################################
-    # TODO:                                                                 #
-    # Calculate the loss value and gradient matrix with linear_loss_..      #
-    #########################################################################
-    loss, grad = None
-    #########################################################################
-    #                       END OF YOUR CODE                                #
-    #########################################################################
-
-    return loss, grad
+        return loss, grad
